36648a8efa0ef1a90f0e7937088cc6d31b51c50f
elvis <adcircus@gmail.com>
2019-04-08 14:13:24 -0500

    clases activas e inactivas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app';
   ver = false;
+  loggedIn = '';
+  registro: string;
+  miArreglo = [1, 2, 3, 4, 5];
 
+  ngOnInit() {
+    this.registro = localStorage.getItem('item');
+  }
   verSpinner() {
     this.ver = true;
     setTimeout(() => {
       this.ver = false;
     }, 5000);
   }
+
+  onClick = () => {
+    localStorage.setItem('item', '5' );
+  }
+
 }
